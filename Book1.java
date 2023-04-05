83dc4b759a9cb109578dcb7c4031c5593040290a
madhavi <madhulakshmi996@gmail.com>
2023-04-05 15:18:32 +0530

    code

---
+++ Book1.java
@@ -0,0 +1,42 @@
+package book;
+
+public class Book1 {
+	String bookName;
+	String authorName;
+	int price;
+	public Book1(String bookName, String authorName, int price) {
+		super();
+		this.bookName = bookName;
+		this.authorName = authorName;
+		this.price = price;
+			}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName=bookName;
+		
+	}
+	public String getAuthorName() {
+		return authorName;
+		
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName=authorName;
+		
+	}
+	public int getPrice() {
+		return price;
+		
+	}
+	
+	public int setPrice(int price) {
+		return this.price=price;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return bookName+","+authorName+","+price;
+	}
+}
